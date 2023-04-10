65f9deedded3ed0204501c5492bc2e98865f34a3
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2023-04-10 19:20:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.promises = [
 
-new Promise(resolve=>setTimeOut(()=>resolve("promise 1 resolved"),Math.floor(Math.random()*5000)+1000)),
+new Promise(resolve=>setTimeOut(()=>resolve("promise 1 resolved"),Math.floor(Math.random()*4000)+1000)),
 
-new Promise(resolve=>setTimeOut(()=>resolve("promise 2 resolved"),Math.floor(Math.random()*5000)+1000)),
+new Promise(resolve=>setTimeOut(()=>resolve("promise 2 resolved"),Math.floor(Math.random()*4000)+1000)),
 	
-new Promise(resolve=>setTimeOut(()=>resolve("promise 3 resolved"),Math.floor(Math.random()*5000)+1000)),
+new Promise(resolve=>setTimeOut(()=>resolve("promise 3 resolved"),Math.floor(Math.random()*4000)+1000)),
 	
-new Promise(resolve=>setTimeOut(()=>resolve("promise 4 resolved"),Math.floor(Math.random()*5000)+1000)),
+new Promise(resolve=>setTimeOut(()=>resolve("promise 4 resolved"),Math.floor(Math.random()*4000)+1000)),
 	
-new Promise(resolve=>setTimeOut(()=>resolve("promise 5 resolved"),Math.floor(Math.random()*5000)+1000)),
+new Promise(resolve=>setTimeOut(()=>resolve("promise 5 resolved"),Math.floor(Math.random()*4000)+1000)),
 	
 ];
 Promise.any(promises).then (result =>{
