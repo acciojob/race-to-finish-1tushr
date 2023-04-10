d9289a78d6b73612e884c73178b323cdfd7e1f06
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2023-04-10 19:13:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-window.promises = [];
+window.promises = [
+
+new Promise(resolve=>setTimeOut(()=>resolve("promise 1 resolved"),Math.floor(Math.random()*5000)+1000)),
+
+new Promise(resolve=>setTimeOut(()=>resolve("promise 2 resolved"),Math.floor(Math.random()*5000)+1000)),
+	
+new Promise(resolve=>setTimeOut(()=>resolve("promise 3 resolved"),Math.floor(Math.random()*5000)+1000)),
+	
+new Promise(resolve=>setTimeOut(()=>resolve("promise 4 resolved"),Math.floor(Math.random()*5000)+1000)),
+	
+new Promise(resolve=>setTimeOut(()=>resolve("promise 5 resolved"),Math.floor(Math.random()*5000)+1000)),
+	
+];
+Promise.any(promises).then (result =>{
+	document.getElementById("output").textContent=result;
+});
 
 // Do not change the code above this
 // add your promises to the array `promises`
